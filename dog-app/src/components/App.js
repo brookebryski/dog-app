33618bb0b3d2140891a3fa7b2a1163b24399ae02
github username <github email address>
2021-07-17 13:15:13 -0500

    debugged ontermsubmit

--- dog-app/src/components/App.js
+++ dog-app/src/components/App.js
@@ -3,14 +3,20 @@ import SearchBar from './SearchBar';
 import dog from '../apis/dog';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        dog.get(`/breed/${term}/images`)
+    state = { pics: [] };
+
+    onTermSubmit = async term => {
+     const response = await dog.get(`/breed/${term}/images`)
+
+     this.setState({ pics: response.data.message });
+ 
     }
        
     render() {
         return (
         <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.pics.length} pictures.
         </div>
         );
     }
