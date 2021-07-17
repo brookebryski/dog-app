e33355e15b308ec5e979b512b50014401bedbe6f
github username <github email address>
2021-07-17 12:59:18 -0500

    axios and dog api

--- dog-app/src/components/App.js
+++ dog-app/src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import dog from '../apis/dog';
 
 class App extends React.Component {
+    onTermSubmit = (term) => {
+        dog.get(`/breed/${term}/images`)
+    }
+       
     render() {
         return (
         <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
         </div>
         );
     }
