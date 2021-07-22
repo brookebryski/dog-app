8b525b69629e6ddea95295585b6e09f85b46bb3a
github username <github email address>
2021-07-22 12:35:31 -0500

    returned images for breed search

--- dog-app/src/components/BreedList.js
+++ dog-app/src/components/BreedList.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const BreedList = (props) => {
-    return <div>{props.pics.length}</div>
+const BreedList = props => {
+  const pics = props.pics.map((pic) => {
+    return <img src={pic} ></img>
+  });
+
+    return <div>{pics}</div>
 };
 
 export default BreedList;
